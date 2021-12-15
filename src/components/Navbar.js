e4d6242f75d8ddf253123772ed95ea3836e4b563
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-12-15 19:46:43 +0600

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,69 @@
-import React from 'react'
-import './Navbar.scss'
+import React, { useState } from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
     return (
-        <div className='navbar-container'>
-            
+        <div className="navbar-container">
+            <div className="navbar-items">
+                <div className="logo">
+                    <img src="./images/Logo.png" alt="logo"></img>
+                    <div className="menu-icon" onClick={handleClick}>
+                    <i
+                        className={
+                            click ? "fas fa-times times" : "fas fa-bars bars"
+                        }
+                    ></i>
+                    {/* <i class="fas fa-bars bars"></i>
+                    <i class="fas fa-times times"></i> */}
+                </div>
+                </div>
+                <div
+                    className={
+                        click
+                            ? "nav-items-container-mobile"
+                            : "nav-items-container"
+                    }
+                >
+                    <ul>
+                        <li>
+                            <Link to="/" className="nav-item">
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="nav-item">
+                                About us
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="nav-item">
+                                Gallery
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="nav-item">
+                                Testimonial
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="nav-item">
+                                Blogs
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="nav-item">
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Welcome from '../Welcome'
 import '../../App.scss'
+import Navbar from '../Navbar'
 
 function Home() {
     return (
         <div>
         <Welcome />
+        <Navbar />
             
         </div>
     )
